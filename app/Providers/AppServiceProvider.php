ea50002275cd8a657c1c90ed098d3d3b9e854a0a
Omar Abdeljelil <omarabdeljelil@gmail.com>
2019-12-01 11:41:06 -0500

    fix migrations max key length error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // fix migrations' max key length error
+        Schema::defaultStringLength(191);
     }
 }
